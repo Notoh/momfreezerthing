07897c751b48cd2ee4628e50dc8b23815a49f7a0
Alex Pawelko <notoh@podcrash.co>
2017-12-20 21:02:04 -0700

    Initial commit

---
+++ src/main/java/io/notoh/mom/gui/MomGui.java
@@ -0,0 +1,12 @@
+package io.notoh.mom.gui;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class MomGui {
+    private JButton checkExpiry;
+    private JLabel selectOption;
+    private JTextField insertItemToAddTextField;
+    private JButton addItemButton;
+}
